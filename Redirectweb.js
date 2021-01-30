6b6886685e84c8ce28a045f1016d1a913d2bd924
hayattgd <hayattgd@gmail.com>
2021-01-30 21:06:03 +0900

    change game name

--- Redirectweb.js
+++ Redirectweb.js
@@ -1,1 +1,7 @@
-location.href = "en-us/index.html"
+if(location.search == "?lang=en-us") {
+    location.href = "en-us/index.html"
+}else if(location.search == "?lang=ja-jp") {
+    location.href = "ja-jp/index.html"
+}else {
+    location.href = "en-us/index.html"
+}
